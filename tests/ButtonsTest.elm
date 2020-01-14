4faa36f1a487e9e5caf25a27850c99715b44c30a
Nelis Boucké <nelis@archiwise.com>
2020-01-14 22:03:19 +0200

    Mastering fuzzing

--- tests/ButtonsTest.elm
+++ tests/ButtonsTest.elm
@@ -1,6 +1,7 @@
 module ButtonsTest exposing (..)
 
 import Expect exposing (Expectation)
+import Fuzz exposing (int)
 import Test exposing (..)
 import Buttons exposing (update, Msg)
 
@@ -8,13 +9,18 @@ suite : Test
 suite =
     describe "Update function"
     [test "increment does +1" <|
-      \_ ->
-        1
-        |> update Buttons.Increment
-        |> Expect.equal 2
-      , test "decrement does -1" <|
-      \_ ->
-        1
-        |> update Buttons.Decrement
-        |> Expect.equal 0
-      ]
+        \_ ->
+            1
+            |> update Buttons.Increment
+            |> Expect.equal 2
+    , test "decrement does -1" <|
+        \_ ->
+            1
+            |> update Buttons.Decrement
+            |> Expect.equal 0
+    , fuzz int "increment" <|
+        \original ->
+            original
+            |> update Buttons.Increment
+            |> Expect.equal (original + 1)
+    ]
